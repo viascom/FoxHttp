c8647fae26fd9867ac9cd92c564ab451bcefb4ab
Patrick Bösch <patrick.boesch@viascom.ch>
2017-05-25 00:52:59 +0200

    Allow the use of custom Gson

--- foxhttp/src/main/java/ch/viascom/groundwork/foxhttp/parser/GsonParser.java
+++ foxhttp/src/main/java/ch/viascom/groundwork/foxhttp/parser/GsonParser.java
@@ -1,22 +1,27 @@
 package ch.viascom.groundwork.foxhttp.parser;
 
 import com.google.gson.Gson;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 /**
  * @author patrick.boesch@viascom.ch
  */
+@AllArgsConstructor
+@NoArgsConstructor
 public class GsonParser implements FoxHttpParser {
+
+    Gson gson = new Gson();
+
     @Override
     public Serializable serializedToObject(String json, Class<Serializable> type) {
-        Gson gson = new Gson();
         return gson.fromJson(json, type);
     }
 
     @Override
     public String objectToSerialized(Serializable o) {
-        Gson gson = new Gson();
         return gson.toJson(o);
     }
 }
