f70b101ba1895518b38c706d4b56fc99513c0863
Patrick Bösch <patrick.boesch@viascom.ch>
2017-05-24 19:04:58 +0200

    Add Javadoc

--- foxhttp/src/main/java/ch/viascom/groundwork/foxhttp/annotation/types/QueryName.java
+++ foxhttp/src/main/java/ch/viascom/groundwork/foxhttp/annotation/types/QueryName.java
@@ -3,6 +3,8 @@ package ch.viascom.groundwork.foxhttp.annotation.types;
 import java.lang.annotation.*;
 
 /**
+ * The @QueryName annotation defines the name of an attribute if a object gets parsed bei the @QueryObject annotation.
+ *
  * @author patrick.boesch@viascom.ch
  */
 @Documented
--- foxhttp/src/main/java/ch/viascom/groundwork/foxhttp/annotation/types/QueryObject.java
+++ foxhttp/src/main/java/ch/viascom/groundwork/foxhttp/annotation/types/QueryObject.java
@@ -3,6 +3,13 @@ package ch.viascom.groundwork.foxhttp.annotation.types;
 import java.lang.annotation.*;
 
 /**
+ * The @QueryObject annotation defines an object which will be parsed and
+ * used as part of the URL path.
+ *
+ * The value is either:
+ * - an array of attribute names which are included in the result
+ * - empty and in this case all attributes are included
+ *
  * @author patrick.boesch@viascom.ch
  */
 @Documented
